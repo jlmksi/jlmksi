8970683c19d4958085d38309f482d6093d5709a8
jlmksi <jlmksi7@gmail.com>
2024-06-06 05:23:57 +0900

    Create  firebase-config.js

---
+++ scripts/firebase-config.js
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCtRN3QMnHxLQEKAAn0y3q8qRvjDs8BKJA",
+    authDomain: "restaurant-orders-new.firebaseapp.com",
+    projectId: "restaurant-orders-new",
+    storageBucket: "restaurant-orders-new.appspot.com",
+    messagingSenderId: "500015809437",
+    appId: "1:500015809437:web:49d6b18dbea33114d06574",
+    measurementId: "G-79XGTK3L3F"
+};
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
+const db = firebase.firestore();
